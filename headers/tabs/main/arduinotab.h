c067c51e1de048ea495c8691deea88735a273ec2
Ghettovich <mrfunkyman@pm.me>
2019-08-24 17:24:01 +0200

    Added button that sends a udp message to an arduino. Arduino turns the led strip on and of through a web relay.

--- headers/tabs/main/arduinotab.h
+++ headers/tabs/main/arduinotab.h
@@ -5,6 +5,7 @@
 #include <QPlainTextEdit>
 #include <QPushButton>
 #include <QTabWidget>
+#include <QTimer>
 #include <QUdpSocket>
 
 class ArduinoTab : public QTabWidget
@@ -16,17 +17,28 @@ public:
 
 private slots:
     void processPendingDatagrams();
+    void startBroadcasting();
+    void broadcastDatagram();
+    void btnClickLED1();
+    //void btnClickLED2();
+    //void btnClickLED3();
 
 private:
     quint16 port = 9900;
+    int msgNr = 1;
     bool isListening = false;
+    bool isLED1_ON = false;
 
     QLabel *lblPingDevice = nullptr;
     QPushButton *btnPingArduino = nullptr;
+    QPushButton *btnLED1 = nullptr;
+    QPushButton *btnLED2 = nullptr;
+    QPushButton *btnLED3 = nullptr;
     QUdpSocket *udpSocket = nullptr;
     QPlainTextEdit *textEditMsg = nullptr;
+    QTimer timer;
 
-    void initSocket();
+    void initSocket();    
     void createButtons();
     void createLabels();
     void createPlainTextFields();
--- tabs/main/arduinotab.cpp
+++ tabs/main/arduinotab.cpp
@@ -15,7 +15,14 @@ void ArduinoTab::createButtons()
     btnPingArduino = new QPushButton("Ping arduino 1", this);
     btnPingArduino->move(30, 30);
     btnPingArduino->setMinimumWidth(150);
-    //btnPingArduino->setMinimumHeight(80);
+    connect(btnPingArduino, &QPushButton::clicked, this, &ArduinoTab::startBroadcasting);
+
+    btnLED1 = new QPushButton("Turn on LED 1", this);
+    btnLED1->move(200, 30);
+    connect(btnLED1, &QPushButton::clicked, this, &ArduinoTab::btnClickLED1);
+
+//    btnLED2 = new QPushButton("Turn on LED 2");
+//    btnLED3 = new QPushButton("Turn on LED 3");
 }
 
 void ArduinoTab::createLabels()
@@ -36,16 +43,17 @@ void ArduinoTab::createPlainTextFields()
 
 void ArduinoTab::initSocket()
 {
+    // CHANGED PORT FOR ARDUINO TEMP
+    quint16 port1 = 10000;
     udpSocket = new QUdpSocket(this);
-    udpSocket->bind(port, QUdpSocket::ShareAddress);
+    udpSocket->bind(port1, QUdpSocket::ShareAddress);
     connect(udpSocket, SIGNAL(readyRead()), this, SLOT(processPendingDatagrams()));
+    connect(&timer, &QTimer::timeout, this, &ArduinoTab::broadcastDatagram);
 }
 
 void ArduinoTab::processPendingDatagrams()
 {
-    //const QString *msg;
     QByteArray datagram;
-//! [2]
     while (udpSocket->hasPendingDatagrams()) {
         datagram.resize(int(udpSocket->pendingDatagramSize()));
         udpSocket->readDatagram(datagram.data(), datagram.size());
@@ -54,5 +62,40 @@ void ArduinoTab::processPendingDatagrams()
         qInfo() << "Message: " << QString(datagram.constData());
         //qDebug() << "message:";
     }
-//! [2]
+}
+
+void ArduinoTab::startBroadcasting()
+{
+    btnPingArduino->setDisabled(true);
+    timer.start(1000);
+}
+
+void ArduinoTab::broadcastDatagram()
+{
+    quint16 clientPort = 9901;
+    setStatusTip(tr("Broadcasting msg %1").arg(msgNr));
+
+    QByteArray datagram = "vind arduino 1";
+    udpSocket->writeDatagram(datagram, QHostAddress::Broadcast, clientPort);
+
+    msgNr++;
+}
+
+void ArduinoTab::btnClickLED1()
+{
+    QByteArray datagram = "";
+    quint16 clientPort = 10000;
+
+    if(isLED1_ON)
+    {
+        datagram = "TURN-LED1-OFF";
+        btnLED1->setText("Turn on");
+    }
+    else
+    {
+        datagram = "TURN-LED1-ON";
+        btnLED1->setText("Turn off");
+    }
+    isLED1_ON = !isLED1_ON;
+    udpSocket->writeDatagram(datagram, QHostAddress::Broadcast, clientPort);
 }
